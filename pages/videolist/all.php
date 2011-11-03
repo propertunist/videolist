4df99e6a3803f735d6cfa35d66e0b52c3b946255
Sem <sembrestels@riseup.net>
2011-11-03 21:54:45 +0100

    Upgraded "all videos" page.

--- pages/videolist/all.php
+++ pages/videolist/all.php
@@ -1,44 +1,49 @@
 <?php
 /**
- * Elgg Video Plugin
- * This plugin allows users to create a library of youtube/vimeo/metacafe videos
+ * All files
  *
- * @package Elgg
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @author Prateek Choudhary <synapticfield@gmail.com>
- * @copyright Prateek Choudhary
+ * @package ElggFile
  */
 
-// Render the video upload page
-// Load Elgg engine
-require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+elgg_push_breadcrumb(elgg_echo('videolist'));
 
-// Get the current page's owner
-$page_owner = page_owner_entity();
-if ($page_owner === false || is_null($page_owner)) {
-	$page_owner = $_SESSION['user'];
-	set_page_owner($_SESSION['guid']);
-}
+elgg_register_title_button();
 
-$title = sprintf(elgg_echo("videolist:search"));
-// get the filter menu
-$friend_link = elgg_get_site_url() . "videolist/friends/" . $page_owner->username;
-// Get objects
-$area1 = elgg_view('page_elements/content_header', array('context' => "everyone", 'type' => 'videolist', 'friend_link' => $friend_link));
-set_input('show_viewtype', 'all');
-$area2 .= elgg_list_entities(array('types' => 'object', 'subtypes' => 'videolist', 'limit' => 10, 'full_view' => TRUE, 'view_type_toggle' => FALSE, 'pagination' => TRUE));
-// include a view for plugins to extend
-$area3 .= elgg_view("videolist/sidebar", array("object_type" => 'videolist'));
+$limit = get_input("limit", 10);
+
+$title = elgg_echo('videolist:all');
+
+$content = elgg_list_entities(array(
+	'types' => 'object',
+	'subtypes' => 'videolist',
+	'limit' => $limit,
+	'full_view' => FALSE
+));
 
 // get the latest comments on all videos
-$comments = get_annotations(0, "object", "videolist", "generic_comment", "", 0, 4, 0, "desc");
-$area3 .= elgg_view('annotation/latest_comments', array('comments' => $comments));
+$comments = elgg_get_annotations(array(
+	'type' => 'object',
+	'subype' => 'videolist',
+	'annotation_names' => array('generic_comment'),
+	'limit' => 4,
+	'order_by' => 'time_created desc',
+));
+$sidebar = elgg_view('annotation/latest_comments', array('comments' => $comments));
 
 // tag-cloud display
-$area3 .= display_tagcloud(0, 50, 'tags', 'object', 'videolist');
+$sidebar .= elgg_view_tagcloud(array(
+	'type' => 'object',
+	'subtype' => 'videolist',
+	'limit' => 50,
+));
 
-set_context('videolist');
-$body = elgg_view_layout('one_column_with_sidebar', $area1.$area2, $area3);
+elgg_set_context('videolist');
+$body = elgg_view_layout('content', array(
+	'filter_context' => 'all',
+	'content' => $content,
+	'title' => $title,
+	'sidebar' => $sidebar,
+));
 
 // Finally draw the page
-page_draw($title, $body);
+echo elgg_view_page($title, $body);
