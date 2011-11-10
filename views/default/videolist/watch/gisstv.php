7d56efa97981052c08683b13c372c465db0b3e1b
Sem <sembrestels@riseup.net>
2011-11-10 23:15:02 +0100

    No autoplay in giss.tv videos.

--- views/default/videolist/watch/gisstv.php
+++ views/default/videolist/watch/gisstv.php
@@ -4,6 +4,6 @@ $video_id = $vars['entity']->video_id;
 $width = $vars['width'];
 $height = $vars['height'];
 
-echo "<video width=\"$width\" height=\"$height\" controls=\"\" autoplay=\"\" tabindex=\"0\">
+echo "<video width=\"$width\" height=\"$height\" controls=\"\" tabindex=\"0\">
 	<source type=\"video/ogg\" src=\"http://giss.tv/dmmdb//contents/$video_id\"></source>
 </video>";
